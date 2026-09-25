5d307779c9170a36e0e7f5483f14cb1cf641c6d7
mohammad mahdi kordbacheh <m09332923881mk@gmail.com>
2026-09-25 11:15:29 +0330

    set admin

--- digital_project/settings.py
+++ digital_project/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -110,8 +112,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
 
 
 #media
--- product/models.py
+++ product/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
     title = models.CharField(_('title'),max_length=50)
     description = models.TextField(_('description'),blank=True)
     avatar = models.ImageField(_('avatar'),blank=True,upload_to='categories')
-    is_enabled = models.BooleanField(_('is enabled'), default=True)
+    is_enable = models.BooleanField(_('is enabled'), default=True)
     created_time = models.DateTimeField(_('created time'),auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time') , auto_now=True)
 
@@ -19,6 +19,10 @@ class Category(models.Model):
         db_table = 'categories'
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
+
+
+    def __str__(self):
+        return f'{self.pk}_{self.title}'
 
 class Products(models.Model):
     title = models.CharField(_('title'), max_length=50)
